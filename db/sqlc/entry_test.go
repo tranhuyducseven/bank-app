70129ecf5f17e53743ba9f87f6a559df2a184828
tranhuyducseven <tranhuyducseven@gmail.com>
2022-04-27 11:50:39 +0700

    [Testing] create Transfer model & test random create, get, list transfer

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -38,6 +38,8 @@ func TestGetEntry(t *testing.T) {
 	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
 	require.NoError(t, err)
 	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
 	require.Equal(t, entry1.AccountID, entry2.AccountID)
 	require.Equal(t, entry1.Amount, entry2.Amount)
 	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
