de56d5e4bae64d63575690e4150766f7475b20b8
tranhuyducseven <tranhuyducseven@gmail.com>
2022-10-19 17:35:50 +0700

    [CRUD]: delete account

--- api/account.go
+++ api/account.go
@@ -63,6 +63,7 @@ func (server *Server) getAccount(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, account)
 }
 
+// LIST
 type listAccountRequest struct {
 	PageID   int32 `form:"page_id" binding:"required,min=1"`
 	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
@@ -86,4 +87,24 @@ func (server *Server) listAccount(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, accounts)
 
+}
+
+//DELETE
+
+type deleteAccountRequest struct {
+	ID int64 `form:"delete_id" binding:"required,min=1"`
+}
+
+func (server *Server) deleteAccount(ctx *gin.Context) {
+	var req deleteAccountRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	err := server.store.DeleteAccount(ctx, req.ID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, req.ID)
 }
--- api/server.go
+++ api/server.go
@@ -17,6 +17,7 @@ func NewServer(store *db.Store) *Server {
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
+    router.DELETE("/accounts", server.deleteAccount)
 
 	server.router = router
 	return server
