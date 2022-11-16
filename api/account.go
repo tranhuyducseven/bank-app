44889c1d1a9cb5963c2629629c91b759cb7d0a8d
tranhuyducseven <tranhuyducseven@gmail.com>
2022-11-16 15:21:00 +0700

    feature(user): model, handler, mock, api

--- api/account.go
+++ api/account.go
@@ -2,9 +2,11 @@ package api
 
 import (
 	"database/sql"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 	db "github.com/tranhuyducseven/Go-bank-app/db/sqlc"
 )
 
@@ -32,11 +34,18 @@ func (server *Server) createAccount(ctx *gin.Context) {
 	}
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
-	ctx.JSON(http.StatusOK, account)
 
+	ctx.JSON(http.StatusOK, account)
 }
 
 // GET
